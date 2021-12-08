57907b2f935940eb6e5dda81a0ff5e4327c1a617
Felipe <luizfelper@gmail.com>
2021-12-08 09:39:38 -0300

    Criação da funcionalidade de clickar nos chats e atualizar a const e mostrar na tela principal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import ChatListItem from './components/ChatListItem';
+import ChatIntro from './components/ChatIntro';
+import ChatWindow from './components/ChatWindow';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search'
 
 export default () => {
-  
-  const [chatlist, setChatList] = useState([{name: 'Felipe'}, {}, {}, {}]);
+
+  const [chatlist, setChatList] = useState([
+    { chatId: 1, title: 'Felipe', image: 'https://www.w3schools.com/w3images/avatar2.png' },
+    { chatId: 2, title: 'Felipe', image: 'https://www.w3schools.com/w3images/avatar2.png' },
+    { chatId: 3, title: 'Felipe', image: 'https://www.w3schools.com/w3images/avatar2.png' },
+    { chatId: 4, title: 'Felipe', image: 'https://www.w3schools.com/w3images/avatar2.png' }
+  ]);
+  const [activeChat, setActiveChat] = useState({});
 
   return (
     <div className="app-window">
@@ -17,31 +25,39 @@ export default () => {
           <img className="header--avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt=""></img>
           <div className="header--buttons">
             <div className="header--btn">
-              <DonutLargeIcon style={{color: '#919191'}}/>
+              <DonutLargeIcon style={{ color: '#919191' }} />
             </div>
             <div className="header--btn">
-              <ChatIcon style={{color: '#919191'}}/>
+              <ChatIcon style={{ color: '#919191' }} />
             </div>
             <div className="header--btn">
-              <MoreVertIcon style={{color: '#919191'}}/>
+              <MoreVertIcon style={{ color: '#919191' }} />
             </div>
           </div>
         </header>
         <div className="search">
           <div className="search--input">
-            <SearchIcon fontSize="small" style={{color: '#919191'}}/>
+            <SearchIcon fontSize="small" style={{ color: '#919191' }} />
             <input type="search" placeholder="Procurar ou começar uma nova conversa" />
           </div>
         </div>
         <div className="chatlist">
-          {chatlist.map((item, key)=>(
+          {chatlist.map((item, key) => (
             <ChatListItem
-            key={key} />
+              key={key}
+              onClick={() => setActiveChat(chatlist[key])}
+            />
           ))}
         </div>
       </div>
+
       <div className="contentarea">
-        ...
+            {activeChat.chatId !== undefined &&
+              <ChatWindow />
+            }
+            {activeChat.chatId === undefined &&
+              <ChatIntro />
+            }
       </div>
     </div>
   )
---
+++ src/components/ChatIntro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ChatIntro.css';
+
+export default () => {
+    return (
+        <div className="chatIntro">
+            
+            <img src="https://i.ibb.co/WGDzNR2/a.png" alt=""/>
+            <h1>Mantenha seu celular conectado.</h1>
+            <h2>O WhatsApp conecta ao seu telefone para sincronizar suas mensagens.
+                Para reduzir o uso de dados, conecte seu telefone a uma rede Wi-Fi.</h2>
+        </div>
+    )
+}
