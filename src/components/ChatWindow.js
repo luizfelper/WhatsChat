556ca87732437684daa9e053264fb95cfed1a3ea
Felipe <luizfelper@gmail.com>
2021-12-18 20:01:26 -0300

    Estilização do footer do ChatWindow

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -4,6 +4,10 @@ import './ChatWindow.css';
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
 
 export default () => {
     return (
@@ -30,7 +34,22 @@ export default () => {
 
             </div>
             <div className="chatWindow--footer">
-
+                <div className="chatWindow--pre">
+                    <div className="chatWindow--btn">
+                        <InsertEmoticonIcon style={{ color: '#919191' }}/>
+                    </div>
+                </div>
+                <div className="chatWindow--inputarea">
+                    <input
+                    className="chatWindow--input"
+                    type="text"
+                    placeholder="Digite uma mensagem..."/>
+                </div>
+                <div className="chatWindow--pos">
+                    <div className="chatWindow--btn">
+                        <SendIcon style={{ color: '#919191' }}/>
+                    </div>
+                </div>
             </div>
         </div>
     )
